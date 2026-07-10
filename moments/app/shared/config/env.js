d9bc3780cc5c91ecaf8a4776a7125037e93fd43e
ertpr99 <ert.pr99@gmail.com>
2026-07-10 12:50:19 +0200

    Delete moments directory

--- moments/app/shared/config/env.js
+++
@@ -1,5 +0,0 @@
-export const env = {
-  "SUPABASE_URL": "https://krrvrvvelgqzbtoeqslt.supabase.co",
-  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtycnZydnZlbGdxemJ0b2Vxc2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0Mjg0NTEsImV4cCI6MjA5OTAwNDQ1MX0.3BtnCNogffiOqVTEc0cyBBvTjZsvgIdrjWLqpEYnDT4",
-  "STORAGE_BUCKET_NAME": "moments"
-};
--- moments/backend-placeholder/README.md
+++
@@ -1,5 +0,0 @@
-# Backend
-
-Moments test surumu Supabase Auth, Database, Storage ve Realtime kullanacak sekilde hazirlandi.
-
-Sunucu tarafi secret islemler icin `SUPABASE_SERVICE_ROLE_KEY` yalnizca server ortaminda tutulmalidir.
