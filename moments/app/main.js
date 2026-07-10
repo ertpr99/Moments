d07db851cb14b9f1450f56cf5e6d6b82e79c371b
ertpr99 <ert.pr99@gmail.com>
2026-07-10 13:00:09 +0200

    Delete moments directory

--- moments/app/main.js
+++
@@ -1,7 +0,0 @@
-import { mountMomentsApp } from "./shared/components/AppShell.js";
-import { createAppState } from "./shared/logic/state.js";
-
-const root = document.querySelector("#app-root");
-const state = createAppState("ios");
-
-mountMomentsApp(root, state);
